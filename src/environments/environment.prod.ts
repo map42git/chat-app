5710b8fa0b6a13da566a8d85e6f80a4e92765460
alexander <alex@crm42.com>
2020-07-01 11:22:38 +0300

    upstart

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,16 +11,3 @@ export const environment = {
     measurementId: "G-T1R9LX3WGS",
   },
 };
-
-// export const envFire = {
-//   firebase: {
-//     apiKey: "AIzaSyAqd50KL19k7O-Zj3usDafq9Y7zVZ_4ghQ",
-//     authDomain: "upstartchat.firebaseapp.com",
-//     databaseURL: "https://upstartchat.firebaseio.com",
-//     projectId: "upstartchat",
-//     storageBucket: "upstartchat.appspot.com",
-//     messagingSenderId: "403870248280",
-//     appId: "1:403870248280:web:bb256ecf6ca88d0d3ec92d",
-//     measurementId: "G-T1R9LX3WGS",
-//   },
-// };
