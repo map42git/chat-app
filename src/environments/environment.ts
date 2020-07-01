14f76983dc015abecf7792e1e0fa91a99ecbe7cf
alexander <alex@crm42.com>
2020-07-01 11:24:38 +0300

    ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,6 @@
 export const environment = {
   production: false,
   firebase: {
-    // apiKey: "AIzaSyB204z8VRE12bqJpmfseFCgHqRW-aRxeww",
-    // authDomain: "upstartchat-a40c6.firebaseapp.com",
-    // databaseURL: "https://upstartchat-a40c6.firebaseio.com",
-    // projectId: "upstartchat-a40c6",
-    // storageBucket: "upstartchat-a40c6.appspot.com",
-    // messagingSenderId: "573374342673",
-    // appId: "1:573374342673:web:e6d5e3a9a6cb698bfa73b0",
-    // measurementId: "G-RDXWKBWEH4"
-    // upstart initial
     apiKey: "AIzaSyAqd50KL19k7O-Zj3usDafq9Y7zVZ_4ghQ",
     authDomain: "upstartchat.firebaseapp.com",
     databaseURL: "https://upstartchat.firebaseio.com",
